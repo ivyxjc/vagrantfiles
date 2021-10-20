342125e95b4e5cc3fef9940f6055cc94c40f5ec2
ivyxjc <ivyxjc1994@hotmail.com>
2021-10-20 15:48:03 +0800

    feat(): import bento ubuntu

--- .github/script/vagrant.sh
+++ .github/script/vagrant.sh
@@ -3,6 +3,27 @@ version=$(date +%Y%m%d%H%M)
 box_name=$1
 
 echo -n "$b "
+echo "=============check box exists $box_name============="
+# check box exists
+box_exists=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+  --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+  https://app.vagrantup.com/api/v1/box/ivyxjc/$box_name)
+
+echo -n "$b "
+if [[ "$box_exists" -eq "404" ]]; then
+    echo "=============create box $box_name============="
+    # create box
+    curl \
+      --header "Content-Type: application/json" \
+      --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+      https://app.vagrantup.com/api/v1/boxes \
+      --data "{ \"box\": { \"username\":\"ivyxjc\", \"name\":\"$box_name\", \"is_private\": false, \"short_description\": \"$box_name\",  \"description\": \"$box_name\"} }"
+fi
+if [[ "$box_exists" -eq "200" ]]; then
+    echo "box exists, no need to create box"
+fi
+
+echo -n "$b "
 echo "=============create version $box_name============="
 # create version
 curl \
@@ -35,7 +56,7 @@ echo $upload_path
 
 echo $(pwd)
 ls -la
-curl $upload_path --request PUT --upload-file $box_name.box
+curl $upload_path --request PUT --upload-file $box_name.virtualbox.box
 
 echo -n "$b "
 echo "=============release $box_name============="
